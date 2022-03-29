cd6215208913fc94f45463c02b29f7dec37a3f68
Jason Safro <jason@jsafro.com>
2022-03-29 14:03:00 +0200

    Enhanced the Formatted Text block. Placed several formatted text blocks on login and forgot password pages.

--- web/modules/custom/silvi_mix/src/Plugin/Block/FormattedTextInConfig.php
+++ web/modules/custom/silvi_mix/src/Plugin/Block/FormattedTextInConfig.php
@@ -20,14 +20,24 @@ class FormattedTextInConfig extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    // Get config.
     $config = $this->getConfiguration();
+
+    // Prep CSS classes.
+    $all_css_classes = [];
+    if (!empty($config['common_usage'])) :
+      $all_css_classes[] = 'usage-'.$config['common_usage'];
+    endif;
+    if (!empty($config['css_classes'])) :
+      $all_css_classes[] = $config['css_classes'];
+    endif;
 
     return [
       '#type' => 'markup',
       '#markup' => $config['formatted_text'],
       '#attributes' => [
         'class' => [
-          $config['css_classes'] ?? '',
+          implode(' ', $all_css_classes),
         ],
       ],
     ];
@@ -49,6 +59,20 @@ class FormattedTextInConfig extends BlockBase {
       '#format' => $config['formatted_text_format'] ?? 'full_html',
     ];
 
+    $form['common_usage'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Common Uses'),
+      '#description' => $this->t('Is this block intended for one of the following standard uses?'),
+      '#default_value' => $config['common_usage'] ?? 'no',
+      '#options' => [
+        'no' => 'No, this block is not intended for a standard use',
+        'full_width_banner' => 'Full width, banner',
+        'half_width_text_on_image' => 'Half width, text overlaid on an image',
+        'half_width_text_on_solid' => 'Half width, text overlaid on a solid background',
+      ],
+      '#required' => TRUE,
+    ];
+
     $form['css_classes'] = [
       '#type' => 'textfield',
       '#title' => $this->t('CSS Classes'),
@@ -67,6 +91,7 @@ class FormattedTextInConfig extends BlockBase {
     $values = $form_state->getValues();
     $this->configuration['formatted_text'] = $values['formatted_text']['value'];
     $this->configuration['formatted_text_format'] = $values['formatted_text']['format'];
+    $this->configuration['common_usage'] = $values['common_usage'];
     $this->configuration['css_classes'] = $values['css_classes'];
   }
 
