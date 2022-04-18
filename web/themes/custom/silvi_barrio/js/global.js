3fdc6978ca486feba06557f88aa085b310f678be
stko-in <staskotliar.dev@gmail.com>
2022-04-18 14:33:09 +0300

    Added new custom theme

---
+++ web/themes/custom/silvi_barrio/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.silvi_barrio = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
